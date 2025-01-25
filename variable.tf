7ac97fc1bea205417d8f6bdc8ed3f607085b20b1
Chandan-3461 <gosavi.chandan.18ce1046@gmail.com>
2025-01-25 22:58:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,10 @@ terraform {
     }
   }
 }
+
+# AWS Provider Configuration
+provider "aws" {
+  region = "ap-south-1"
+  # Credentials will be automatically picked up from the environment variables:
+  # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+}
