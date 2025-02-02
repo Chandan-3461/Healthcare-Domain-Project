d03535cf508c25f081fd556dd90807df0216a253
Chandan-3461 <gosavi.chandan.18ce1046@gmail.com>
2025-02-02 11:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test_cluster" {
     ami           = "ami-00bb6a80f01f03502"
-    instance_type = "t2.micro"
+    instance_type = "t3.medium"
     count         = 1
     tags = {
         Name = "Production_server"
