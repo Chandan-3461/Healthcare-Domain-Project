a8ab9c3bedf598c8a65bb9e4349f790988995f1b
Chandan-3461 <gosavi.chandan.18ce1046@gmail.com>
2025-02-09 11:45:44 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "test_cluster" {
-    ami           = "ami-00bb6a80f01f03502"
-    instance_type = "t3.medium"
-    count         = 1
-    tags = {
-        Name = "Production_server"
-    }
-}
-
